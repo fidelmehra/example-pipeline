952bfef2ecd7d4c19c686306c23951b2365d05f0
Fidel <fidel.mehra@gmail.com>
2026-05-21 11:45:41 +0100

    Create logging.py

---
+++ example_pipeline/utils/logging.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        fmt = logging.Formatter(
+            "[%(asctime)s] %(levelname)s - %(name)s - %(message)s"
+        )
+        ch.setFormatter(fmt)
+        logger.addHandler(ch)
+    return logger
